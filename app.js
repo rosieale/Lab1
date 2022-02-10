3f1a50a7372bf0bee8c049e45d1b0581106b8453
Rosie-Maldonado <rosiefordummies@gmail.com>
2022-02-09 16:38:50 -0800

    3000

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+} )
+
+app.listen(process.env.PORT, ()=>console.log("El puerto esta corriendo el puerto TCP" + process.env.PORT))
